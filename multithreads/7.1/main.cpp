10928b0ebcbb58bc44de83deee74741363941418
Evgeny <evgeny.kochenyuk@gmail.com>
2020-11-22 04:35:45 +0300

    7.1 save pid to file

--- multithreads/7.1/main.cpp
+++ multithreads/7.1/main.cpp
@@ -1,4 +1,7 @@
 #include <pthread.h>
+#include <stdio.h>
+       #include <sys/types.h>
+       #include <unistd.h>
 
 
 void *f(void*)
@@ -11,7 +14,9 @@ int main()
 {
 	pthread_t thread_id;
 	pthread_create (&thread_id, NULL, f, NULL);
-
+	FILE *f = fopen("/home/box/main.pid", "w");
+	fprintf(f, "%d", getpid());
+	fclose(f);
 	pthread_join(thread_id, NULL);
 }
 
