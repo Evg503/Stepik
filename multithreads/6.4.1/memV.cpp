a3259886c8d1262438ac7e47ce7903955fd18e2f
Evgeny <evgeny.kochenyuk@gmail.com>
2020-11-22 03:26:11 +0300

    6.4.1 share 1M memory

--- multithreads/6.4.1/memV.cpp
+++ multithreads/6.4.1/memV.cpp
@@ -8,7 +8,7 @@
 int main()
 {
 	key_t key = ftok("/tmp/mem.temp", 1);
-	size_t size = 1<<10;
+	size_t size = 1<<20;
 	int memd = shmget(key, size, IPC_CREAT | 0666);
 	void *p = shmat(memd, 0, 0);
 	if (p != (void*) -1)
