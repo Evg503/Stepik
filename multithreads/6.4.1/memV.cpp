32848208d04cdaee1b7107172b061b8038b6d0b6
Evgeny <evgeny.kochenyuk@gmail.com>
2020-11-22 03:21:13 +0300

    6.4.1 SysV shared memory

---
+++ multithreads/6.4.1/memV.cpp
@@ -0,0 +1,19 @@
+       #include <sys/ipc.h>
+       #include <sys/shm.h>
+
+#include <string.h>
+#include <stdio.h>
+
+
+int main()
+{
+	key_t key = ftok("/tmp/mem.temp", 1);
+	size_t size = 1<<10;
+	int memd = shmget(key, size, IPC_CREAT | 0666);
+	void *p = shmat(memd, 0, 0);
+	if (p != (void*) -1)
+		memset(p, 42, size);
+	else
+		printf("error\n");
+}
+
