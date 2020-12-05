19e89ad9891ee8a4f9550e0fa83a36c609a67d7f
Evgeny <evgeny.kochenyuk@gmail.com>
2020-12-05 20:31:45 +0300

    added threading for each accept

--- multithreads/9.1/main.cpp
+++ multithreads/9.1/main.cpp
@@ -1,6 +1,11 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include <thread>
 
 struct Config {
 
@@ -21,6 +26,44 @@ void print_config()
 	printf(" Is daemon:\"%s\"\n", config.is_daemon?"Daemon":"Not");
 }
 
+void runconnection(int sock)
+{
+
+	printf("Start connection\n");
+	sleep(3);
+	printf("Stop connection\n");
+	close(sock);
+}
+void runserver()
+{
+	int ls_fd;
+
+	ls_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+	struct sockaddr_in address;
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(config.port);
+
+	if (bind(ls_fd, (struct sockaddr *)&address, sizeof(address))<0)
+	{
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+	if (listen(ls_fd, SOMAXCONN) < 0)
+	{
+		perror("listen");
+		exit(EXIT_FAILURE);
+	}
+	int new_sock;
+	while (new_sock = accept(ls_fd, 0, 0))
+	{
+		printf("Accept! %d\n", new_sock);
+		std::thread t(runconnection, new_sock);
+		t.detach();
+	}
+
+}
 int main(int argc, char** argv)
 {
 
@@ -49,7 +92,7 @@ int main(int argc, char** argv)
 	if(config.is_daemon)
 		daemon(0,0);
 
-	sleep(10);
+	runserver();
 	printf("Stop!\n");
 
 }
