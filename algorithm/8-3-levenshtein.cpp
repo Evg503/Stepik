b4942ffaf3c0ea559f44ff51f104a77804afd6db
Evgeny <evgeny.kochenyuk@gmail.com>
2021-01-06 05:25:55 +0300

    alg: 8.3 levenstein + memoize = worked

--- algorithm/8-3-levenshtein.cpp
+++ algorithm/8-3-levenshtein.cpp
@@ -3,21 +3,41 @@
 #include <iostream>
 #include <string>
 
-int get_levenshtein_distance(const std::string &str1, const std::string &str2) {
-  // TODO optimize this function
-  size_t size1 = str1.size();
-  size_t size2 = str2.size();
-  if (str1.empty() || str2.empty()) {
-    return size1 + size2;
-  }
 
-  int insert_distance = get_levenshtein_distance(str1, str2.substr(1)) + 1;
-  int delete_distance = get_levenshtein_distance(str1.substr(1), str2) + 1;
-  int match_distance = get_levenshtein_distance(str1.substr(1), str2.substr(1)) + (str1[0] != str2[0]);
+
+int mem_distance(const std::string &str1, const std::string &str2);
+int get_levenshtein_distance(const std::string &str1, const std::string &str2);
+
+int get_levenshtein_distance(const std::string &str1, const std::string &str2) {
+  int insert_distance = mem_distance(str1, str2.substr(1)) + 1;
+  int delete_distance = mem_distance(str1.substr(1), str2) + 1;
+  int match_distance = mem_distance(str1.substr(1), str2.substr(1)) + (str1[0] != str2[0]);
   return std::min({insert_distance, delete_distance, match_distance});
 }
 
+const int max_len = 128;
+int D[max_len][max_len];
+
+void init_D()
+{
+	for(int i = 0; i < max_len; ++i)
+		for (int j = 0; j < max_len; ++j)
+			D[i][j] = i*j==0?i+j:-1;
+}
+
+
+int mem_distance(const std::string &str1, const std::string &str2)
+{
+	size_t size1 = str1.size();
+	size_t size2 = str2.size();
+	if (D[size1][size2] == -1)
+		D[size1][size2] = get_levenshtein_distance(str1, str2);
+	return D[size1][size2];
+}
+
 int main(void) {
+  init_D();
+
   std::string str1;
   std::string str2;
 
